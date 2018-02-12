b93e3f5ec33dd285dabe9522bab6b83fcbc491d1
Duane Irvin <irvin93d@gmail.com>
2018-02-12 12:02:16 +0100

    Add new game button

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Container} from 'reactstrap';
+import {Container, Row, Col, Navbar, Form, FormGroup, Label, Input, Button} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
@@ -39,36 +39,85 @@ class MineCell extends React.Component {
 }
 
 class MineField extends React.Component {
-  constructor(props) {
-    super(props);
-    this.newGame();
 
-    this.state = 
-      {
-        cells:[],
-        rows: 0,
-        cols: 0
-      }
+  handleClick(row, col, action) {
+    this.props.updateCell(row, col, action)
+  };
+  
+  renderGrid() {
+    const grid = this.props.game || { cells:[], rows: 0, cols: 0 }
+    const size = 100/grid.cols
+    const margin = 1;
+    const divStyle = {
+      margin: margin + 'px',
+      width: 'calc(' + size + '% - ' + margin*2 + 'px)',
+      paddingBottom: size + '%'
+    };
+
+    return grid.cells.map((cell, i) => {
+      const col = i % grid.cols;
+      const row = Math.floor(i/grid.cols);
+      return <MineCell 
+              key={i} divStyle={divStyle} row={row} col={col}
+              flag={cell.flag} open={cell.open} content={cell.content}
+              handleClick={this.handleClick.bind(this)}
+              />
+    });
   }
-  newGame(rows, cols, mines) {
-    fetch('http://127.0.0.1:8080/api/minesweeper',{
-        method: 'POST'
-      })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          result.cells = result.cells.reduce( (a,b) => {
-              return a.concat(b);
-            });
-          this.setState(result);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
+
+  render() {
+    return (
+      <div className='minefield'>
+        {this.renderGrid()}
+      </div>
+    );
   }
+}
+
+
+class SideBar extends React.Component {
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const rows = document.getElementById('rows').value;
+    const cols = document.getElementById('cols').value;
+    const mines = document.getElementById('mines').value;
+    this.props.newGame(rows, cols, mines);
+  }
+
+  render() {
+    return (
+      <Navbar className='sidebar'>
+        <h1> Minesweeper </h1>
+        <Form onSubmit={this.handleSubmit.bind(this)}>
+          <Button color='primary'>New Game</Button>
+          <FormGroup>
+            <Label for="rows">Rows</Label>
+            <Input type="number" name="rows" id="rows" placeholder="Rows" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="cols">Columns</Label>
+            <Input type="number" name="cols" id="cols" placeholder="Columns" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="mines">Mines</Label>
+            <Input type="number" name="mines" id="mines" placeholder="Mines" />
+          </FormGroup>
+        </Form>
+      </Navbar>
+    );
+  }
+}
+
+class MainFrame extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {game: null}
+  }
+
   updateCell(row, col, action) {
-    console.log('Unflaging')
+    console.log(action)
     fetch('http://127.0.0.1:8080/api/minesweeper', {
       method: 'PUT',
       header: {
@@ -88,7 +137,7 @@ class MineField extends React.Component {
         result.cells = result.cells.reduce( (a,b) => {
             return a.concat(b);
           });
-        this.setState(result);
+        this.setState({game:result});
       },
       (error) => {
         console.error(error);
@@ -96,43 +145,47 @@ class MineField extends React.Component {
     );
   }
 
-  handleClick(row, col, action) {
-    this.updateCell(row, col, action)
-  };
-  
-  renderGrid() {
-    const grid = this.state;
-    const size = 100/grid.cols
-    const margin = 1;
-    const divStyle = {
-      margin: margin + 'px',
-      width: 'calc(' + size + '% - ' + margin*2 + 'px)',
-      paddingBottom: size + '%'
-    };
-    return grid.cells.map((cell, i) => {
-      const col = i % grid.cols;
-      const row = Math.floor(i/grid.cols);
-      return <MineCell 
-              key={i} divStyle={divStyle} row={row} col={col}
-              flag={cell.flag} open={cell.open} content={cell.content}
-              handleClick={this.handleClick.bind(this)}
-              />
-    });
+  newGame(rows, cols, mines) {
+    console.log('New game');
+    fetch('http://127.0.0.1:8080/api/minesweeper',{
+        method: 'POST'
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          result.cells = result.cells.reduce( (a,b) => {
+              return a.concat(b);
+            });
+          this.setState({game:result});
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
+
+  handleClick(e) {
+    this.setState({val:'you'})
   }
 
   render() {
     return (
-      <Container>
-        <div className='minefield'>
-          {this.renderGrid()}
-        </div>
+      <Container onClick={this.handleClick.bind(this)} className='mainframe'>
+        <Row>
+          <Col xs='12' lg='4'> 
+            <SideBar newGame={this.newGame.bind(this)} />
+          </Col>
+          <Col xs='12' lg='8'>
+            <MineField game={this.state.game} updateCell={this.updateCell.bind(this)} />
+          </Col>
+        </Row>
       </Container>
     );
-  }
+  } 
 }
 
 ReactDOM.render(
-    <MineField />,
+     <MainFrame />,
     document.getElementById('root')
 );
 
